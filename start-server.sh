9be8672f075e90c4c95158c1ca736781076c7974
karl <50684527+nisheri-ascar@users.noreply.github.com>
2025-05-11 20:12:04 +0800

    remove those for now

--- start-server.sh
+++ start-server.sh
@@ -5,27 +5,6 @@ memory=400M
 JAVA_FLAGS=(
   -Xms${memory}
   -Xmx${memory}
-  --add-modules=jdk.incubator.vector
-  -XX:+UseG1GC
-  -XX:+ParallelRefProcEnabled
-  -XX:MaxGCPauseMillis=200
-  -XX:+UnlockExperimentalVMOptions
-  -XX:+DisableExplicitGC
-  -XX:+AlwaysPreTouch
-  -XX:G1HeapWastePercent=5
-  -XX:G1MixedGCCountTarget=4
-  -XX:InitiatingHeapOccupancyPercent=15
-  -XX:G1MixedGCLiveThresholdPercent=90
-  -XX:G1RSetUpdatingPauseTimePercent=5
-  -XX:SurvivorRatio=32
-  -XX:+PerfDisableSharedMem
-  -XX:MaxTenuringThreshold=1
-  -Dusing.aikars.flags=https://mcflags.emc.gs
-  -Daikars.new.flags=true
-  -XX:G1NewSizePercent=30
-  -XX:G1MaxNewSizePercent=40
-  -XX:G1HeapRegionSize=2M
-  -XX:G1ReservePercent=20
 )
 
 cd server
