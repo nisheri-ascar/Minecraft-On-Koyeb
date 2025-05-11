4516629cd076a4fa50401203dd5b9c0f56cc1b35
nisheri-ascar <kertkert704@gmail.com>
2025-05-11 11:44:50 +0000

    silence wget

--- setup.sh
+++ setup.sh
@@ -8,17 +8,17 @@ vrVersion="4.0.7"
 
 
 # Download Main plugin
-wget https://api.papermc.io/v2/projects/paper/versions/${paperServerVersion}/builds/${paperBuildNumber}/downloads/paper-${paperServerVersion}-${paperBuildNumber}.jar -O server/server.jar
+wget -q https://api.papermc.io/v2/projects/paper/versions/${paperServerVersion}/builds/${paperBuildNumber}/downloads/paper-${paperServerVersion}-${paperBuildNumber}.jar -O server/server.jar
 
 # Download ViaVersion and its friends
 
-wget https://github.com/ViaVersion/ViaVersion/releases/download/${vvVersion}/ViaVersion-${vvVersion}jar -O server/plugins/ViaVersion.jar
+wget -q https://github.com/ViaVersion/ViaVersion/releases/download/${vvVersion}/ViaVersion-${vvVersion}jar -O server/plugins/ViaVersion.jar
 
-wget https://github.com/ViaVersion/ViaBackwards/releases/download/${vbVersion}/ViaBackwards-${vbVersion}.jar -O server/plugins/ViaBackwards.jar
+wget -q https://github.com/ViaVersion/ViaBackwards/releases/download/${vbVersion}/ViaBackwards-${vbVersion}.jar -O server/plugins/ViaBackwards.jar
 
-wget https://github.com/ViaVersion/ViaRewind/releases/download/${vrVersion}/ViaRewind-${vrVersion}.jar -O server/plugins/ViaRewind.jar
+wget -q https://github.com/ViaVersion/ViaRewind/releases/download/${vrVersion}/ViaRewind-${vrVersion}.jar -O server/plugins/ViaRewind.jar
 
 # Download Geyser, we don't need versioning of it yet it depends on ViaVersion to work if server is older/newer
-wget https://download.geysermc.org/v2/projects/geyser/versions/latest/builds/latest/downloads/spigot -O server/Geyser-Spigot.jar
+wget -q https://download.geysermc.org/v2/projects/geyser/versions/latest/builds/latest/downloads/spigot -O server/Geyser-Spigot.jar
 
 
