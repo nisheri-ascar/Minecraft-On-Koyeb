11cade4dbe99cf750b9c9a4a53ba5dc7232bcee2
nisheri-ascar <kertkert704@gmail.com>
2025-05-12 06:37:31 +0000

    disable viaversion plugins for now

--- setup.sh
+++ setup.sh
@@ -16,11 +16,11 @@ wget -q https://api.papermc.io/v2/projects/paper/versions/${paperServerVersion}/
 
 # Download ViaVersion and its friends
 
-wget -q https://github.com/ViaVersion/ViaVersion/releases/download/${vvVersion}/ViaVersion-${vvVersion}jar -O server/plugins/ViaVersion.jar
+#wget -q https://github.com/ViaVersion/ViaVersion/releases/download/${vvVersion}/ViaVersion-${vvVersion}jar -O server/plugins/ViaVersion.jar
 
-wget -q https://github.com/ViaVersion/ViaBackwards/releases/download/${vbVersion}/ViaBackwards-${vbVersion}.jar -O server/plugins/ViaBackwards.jar
+#wget -q https://github.com/ViaVersion/ViaBackwards/releases/download/${vbVersion}/ViaBackwards-${vbVersion}.jar -O server/plugins/ViaBackwards.jar
 
-wget -q https://github.com/ViaVersion/ViaRewind/releases/download/${vrVersion}/ViaRewind-${vrVersion}.jar -O server/plugins/ViaRewind.jar
+#wget -q https://github.com/ViaVersion/ViaRewind/releases/download/${vrVersion}/ViaRewind-${vrVersion}.jar -O server/plugins/ViaRewind.jar
 
 # Download Geyser, we don't need versioning of it yet it depends on ViaVersion to work if server is older/newer
 wget -q https://download.geysermc.org/v2/projects/geyser/versions/latest/builds/latest/downloads/spigot -O server/plugins/Geyser-Spigot.jar
