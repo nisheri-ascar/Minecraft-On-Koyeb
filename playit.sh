6da7821d21d6fd7d9e39893355a6ad11f18fe356
nisheri-ascar <kertkert704@gmail.com>
2025-05-12 05:11:34 +0000

    oversight

--- playit.sh
+++ playit.sh
@@ -5,4 +5,6 @@ if [ -z "$PLAYIT_SECRET_KEY" ]; then
     echo "ERROR!!! PLAYIT_SECRET_KEY IS EMPTY!"
     echo "Refer to readme to fix this error."
     exit 127
+fi
+
 docker run --rm -it --net=host -e SECRET_KEY=${PLAYIT_SECRET_KEY} ghcr.io/playit-cloud/playit-agent:0.15
