751ac3da07a6ae4c7e5146c0d343f6c6b1bc71e5
nisheri-ascar <kertkert704@gmail.com>
2025-05-15 14:28:27 +0800

    add readme and start moving codes

---
+++ server-paper-1.21.5/readme.md
@@ -0,0 +1,11 @@
+# Variables
+
+`SERVER_FLAVOR`: can be `cuberite+viaproxy`/`paper1.8`/`paperLatest`/`bedrock+viaproxy`
+
+`PLAYIT_SECRET_KEY`: playit.gg secret key 
+
+`paperServerVersion`: 
+
+`paperBuildNumber`: 
+
+`playitUrl`: optional
---
+++ server-paper-1.21.5/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+defaultPaperServerVersion="1.21.5"
+defaultPaperBuildNumber=229
+
+if [[ -z paperBuildNumber ]]; then
+    paperBuildNumber=$defaultPaperBuildNumber
+fi
+
+if [[ -z paperServerVersion ]]; then
+  paperServerVersion=$defaultPaperServerVersion
+fi
+
+wget -q https://api.papermc.io/v2/projects/paper/versions/${paperServerVersion}/builds/${paperBuildNumber}/downloads/paper-${paperServerVersion}-${paperBuildNumber}.jar -O server/server.jar
+
