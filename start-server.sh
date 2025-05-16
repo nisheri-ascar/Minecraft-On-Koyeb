a1488160a103b3de393707523b300b9f9eb10120
karl <50684527+nisheri-ascar@users.noreply.github.com>
2025-05-16 13:06:40 +0800

    bring back flags in start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -5,6 +5,14 @@ memory=350M
 JAVA_FLAGS=(
   -Xms${memory}
   -Xmx${memory}
+  -Xss1M 
+  -XX:+UnlockExperimentalVMOptions 
+  -XX:+UseG1GC 
+  -XX:G1NewSizePercent=20 
+  -XX:G1ReservePercent=20 
+  -XX:MaxGCPauseMillis=200
+  -XX:G1HeapRegionSize=32M
+
 )
 
 cd server
