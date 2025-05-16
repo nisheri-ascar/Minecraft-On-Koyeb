e12516c6497bc0191d1d72d126dcbf52385a0ef1
karl <50684527+nisheri-ascar@users.noreply.github.com>
2025-05-16 14:24:23 +0800

    try using vanilla server jar

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ chmod 777 bin/playit-binary
 
 # Download Main jar
 #wget -q https://api.papermc.io/v2/projects/paper/versions/${paperServerVersion}/builds/${paperBuildNumber}/downloads/paper-${paperServerVersion}-${paperBuildNumber}.jar -O server/server.jar
-wget https://api.papermc.io/v2/projects/paper/versions/1.9.4/builds/775/downloads/paper-1.9.4-775.jar -O server/server.jar
+wget https://piston-data.mojang.com/v1/objects/edbb7b1758af33d365bf835eb9d13de005b1e274/server.jar -O server/server.jar
 # Download ViaVersion and its friends
 
 #wget -q https://github.com/ViaVersion/ViaVersion/releases/download/${vvVersion}/ViaVersion-${vvVersion}jar -O server/plugins/ViaVersion.jar
